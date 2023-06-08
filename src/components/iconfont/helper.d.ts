f8ceed7b5ecc5a9ed5a61da511de4244b8037323
leidenglai <leidenglai@qq.com>
2023-06-08 15:16:48 +0800

    feat: 生成文件删除

--- src/components/iconfont/helper.d.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-export declare var useGlobalIconFont: () => { iconfont: string };
--- src/components/iconfont/helper.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const useGlobalIconFont = () => {
-  return {
-    iconfont: `components/iconfont/${process.env.TARO_ENV}/${process.env.TARO_ENV}`,
-  };
-};
-
-// es modules is unavaiable.
-module.exports.useGlobalIconFont = useGlobalIconFont;
