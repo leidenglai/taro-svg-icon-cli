fc1fed6ecd42543a011bceb79312851c3286ef71
Jichen <zhangjichen@oppentech.com>
2025-08-02 21:35:54 +0800

    update commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,7 +6,7 @@ console.log([
   '',
   'Usage:',
   '',
-  '    ' + colors.green.bold('npx iconfont-init [--output]') + '     : Generate configuration file, default file name is iconfont.json',
-  '    ' + colors.green.bold('npx iconfont-taro [--config]') + '     : Generate icon component',
+  '    ' + colors.green.bold('npx taro-svg-generator-init [--output]') + '     : Generate configuration file, default file name is iconfont.json',
+  '    ' + colors.green.bold('npx taro-svg-generator-sync [--config]') + '     : Generate icon component',
   '',
 ].join('\n'));
